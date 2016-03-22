a25f9bcc9aa5d9b4d9dc8d73a549f359b0340509
Apple <tiffine.koch@gmail.com>
2016-03-21 22:31:46 -0700

    working home page

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,4 +5,31 @@ var app = angular.module('myApp');
 app.controller('homeCtrl', function($scope, $http, $state, $stateParams) {
   console.log('homeCtrl working');
 
+  $http({
+    method: "GET",
+    url: "/restaurants"
+    }).then(function(response){
+      $scope.restaurants= response.data;
+    }, function(error){
+      console.log('error');
+  });
+});
+
+app.controller('entryCtrl', function($scope, $http, $state, $stateParams) {
+  console.log('entryCtrl working');
+  $scope.restaurants = [];
+  var restaurant = {Time: $scope.restaurant.Time, Phone: $scope.restaurant.Phone, Name: $scope.restaurant.Name, PartySize: $scope.restaurant.PartySize, Indoor: $scope.restaurant.Indoor, Arrived: $scope.restaurant.Arrived};
+  $scope.restaurants.push(restaurant);
+  $http({
+    method: 'POST',
+    url: '/restaurants',
+    data: restaurant
+    }).then(function(response){
+      swal("Your destination has been uploaded!");
+    }, function(err){
+      console.error(err);
+    })
+    $scope.restaurant = {};
+  };
+
 });
--- public/js/module.js
+++ public/js/module.js
@@ -5,7 +5,7 @@ var app = angular.module('myApp', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', { url: '/', templateUrl: '/partials/home.html', controller: 'homeCtrl'})
-//     .state('destinations', { url: '/destinations', templateUrl: 'partials/destinations.html', controller: 'destCtrl' })
+    .state('entry', { url: '/entry', templateUrl: 'partials/entry.html', controller: 'entryCtrl' })
 //
   $urlRouterProvider.otherwise('/');
 });
