3f3cfb5ca6f3fcb176b02bd8496bbcab5dfe15a8
Apple <tiffine.koch@gmail.com>
2016-03-21 23:51:00 -0700

    delete functionality

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -13,6 +13,36 @@ app.controller('homeCtrl', function($scope, $http, $state) {
     }, function(error){
       console.log('error');
   });
+
+  $scope.deleteRest = function(index, restaurant) {
+    console.log('delete button');
+    var id = restaurant._id
+    console.log('id', id);
+    var deleted = $scope.restaurants.splice(index, 1);
+    $http({
+      method: 'DELETE',
+      url: `/restaurants/${id}`
+    })
+    .then(function(data) {
+      console.log('success');
+    }, function(err) {
+      console.error(err);
+    })
+  }
+  $scope.editRest = function(index, restaurant) {
+    var id = restaurant._id
+    console.log('id', id);
+    var edited = $scope.restaurants.splice(index, 1);
+    $http({
+      method: 'PUT',
+      url: `/restaurants/${id}`
+    })
+    .then(function(data) {
+      console.log('success');
+    }, function(err) {
+      console.error(err);
+    })
+  }
 });
 
 app.controller('entryCtrl', function($scope, $http, $state) {
@@ -34,5 +64,5 @@ app.controller('entryCtrl', function($scope, $http, $state) {
         console.error(err);
       })
       $scope.restaurant = {};
-    }  
+    }
 });
