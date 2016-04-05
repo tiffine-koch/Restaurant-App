2f2b54dbc0c7f78b2d0030b144dae55d7b9d0a6a
Apple <tiffine.koch@gmail.com>
2016-04-04 22:49:32 -0700

    fixing home page

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ app.use(stormpath.init(app, { website: true }));
 app.on('stormpath.ready', function() {
   app.listen(process.env.PORT || 3000);
 });
+
+
+app.get('/home', stormpath.loginRequired, function(req, res) {
+  res.send('You have reached the dashboard page! You must be logged in.');
+});
