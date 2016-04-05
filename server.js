c93245daf49f4463d78d076e8402c7ba5768d464
Apple <tiffine.koch@gmail.com>
2016-04-04 23:00:06 -0700

    testing routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var stormpath = require('express-stormpath');
+// var routes = require('./routes/index');
+var routes = require('./routes/restaurants');
+
+// app.use('/', require('./routes/index'));
+// app.use('/restaurants', require('./routes/restaurants'));
 
 var app = express();
 app.use(stormpath.init(app, { website: true }));
